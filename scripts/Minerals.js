9d0fdda28dd0a9ca4df0c015c369b78a979ad18f
Caleb Pittman <ambassadoor.dev@gmail.com>
2025-10-01 21:21:08 -0500

    Created basic html for displaying current facility minerals

---
+++ scripts/Minerals.js
@@ -0,0 +1,57 @@
+// Import render to rerender page on facility selection
+import { render } from "./main.js"
+
+// Temporary state to hold current facility selection 
+let currentFacilityId
+
+// Sets temporary facility state and rerenders page
+export const updateCurrentFacility = (e) => {
+    currentFacilityId = Number(e.target.value)
+    render()
+}
+
+
+// Sets html for current facility Minerals
+export const Minerals = async () => {
+    // Retrieve facilities and their minerals from the database
+    const response = await fetch("http://localhost:8088/facilities?_embed=facilityMinerals")     
+
+    // Start the mineral html
+    let html = `<h3>Facility Minerals`
+
+    if (response.ok) {    
+        const facilities = await response.json()
+
+        // Check that selection has been made
+        if (currentFacilityId) {
+            // Find the current facility
+            const currentFacility = facilities.find(facility => facility.id === currentFacilityId)
+            let currentMinerals = []
+
+
+                // Adds Facility name to heading
+                html += `
+                    for ${currentFacility.name}</h3>`
+
+                // Retrieves minerals from the database
+                const mineralResponse = await fetch("http://localhost:8088/minerals")
+                
+                if (response.ok) {
+                    const minerals = await mineralResponse.json()     
+
+                    // Create a radio input for each mineral
+                    currentMinerals = currentFacility.facilityMinerals.map(fm => minerals.find(mineral => mineral.id === fm.mineralId))
+                    currentMinerals.forEach(mineral => html += `<input type="radio" id=${mineral.id} name="mineral" value="${mineral.id}"/><label for=${mineral.id}>${mineral.name}</label>`)
+                }
+        }
+    }
+    return html
+}
+
+// TODO
+// Display mineral count next to radio buttons
+// Create radio select handler
+// - Reduce count by 1
+// - Update Cart
+// Clean up event listener, facility select already rerenders
+// Undo changes in main before pushing
